2dd5e074621ce8c70a69ce6b17774c4cfe707df7
Kirill Khudyakov <kirill.hudiakov@chemopin.tech>
2017-09-28 13:59:49 +0300

    add notification example

--- PeriodicTable/PeriodicTable/ViewController.swift
+++ PeriodicTable/PeriodicTable/ViewController.swift
@@ -8,18 +8,61 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+class ViewModel {
+    
+    var info: String?
 
 }
 
+class ViewController: UIViewController {
+
+    @IBOutlet weak var buttonSend: UIButton!
+
+    let myNotification = Notification.Name(rawValue:"MoePoslanie")
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let tapOnButtonSend = UITapGestureRecognizer(target: self, action: #selector(onButtonSendTap))
+        buttonSend.addGestureRecognizer(tapOnButtonSend)
+        
+        configureNotifications()
+    }
+
+}
+
+extension ViewController {
+    
+    func onButtonSendTap(sender: UIGestureRecognizer) {
+        let nc = NotificationCenter.default
+        let model = ViewModel()
+        model.info = "Super puper!!!"
+        
+        nc.post(name: myNotification,
+                object: nil,
+                userInfo:["model": model ])
+    }
+    
+    func configureNotifications() {
+        let nc = NotificationCenter.default
+        nc.addObserver(forName: myNotification, object: nil, queue: nil, using: catchMyNotification)
+    }
+    
+    func catchMyNotification(notification: Notification) -> Void  {
+        guard
+            let userInfo = notification.userInfo,
+            let model = userInfo["model"] as? ViewModel
+        else {
+            return
+        }
+        
+        let alert = UIAlertController(title: "info", message: model.info ?? "", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        present(alert, animated: true)
+        
+        print("catch Moe poslanie: \(model.info ?? "")\n")
+        
+    }
+    
+}

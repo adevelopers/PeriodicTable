33a0e29bfb24adc90b6e006d581354e439253d53
Kirill Khudyakov <kirill.hudiakov@chemopin.tech>
2017-10-02 00:16:04 +0300

    Add RadialGradientView

--- PeriodicTable/PeriodicTable/MainScreen/PeriodicTableViewController.swift
+++ PeriodicTable/PeriodicTable/MainScreen/PeriodicTableViewController.swift
@@ -8,6 +8,23 @@
 
 import UIKit
 
+class RadialGradientView: UIView {
+    
+    override func draw(_ rect: CGRect) {
+        let colors = [#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1).cgColor, UIColor.clear.cgColor]
+        
+        if let context = UIGraphicsGetCurrentContext(),
+           let gradient = CGGradient(colorsSpace: nil, colors: colors as CFArray, locations: nil)
+        {
+            context.drawRadialGradient( gradient, startCenter: center,
+                                        startRadius: 0,
+                                        endCenter: center,
+                                        endRadius: frame.height/1.5, options: CGGradientDrawingOptions.drawsAfterEndLocation)
+        }
+    }
+    
+}
+
 class PeriodicTableViewController: UIViewController {
 
     var tableView: UITableView!

1844970d27aaaa933492f6ee28aff3d0f3d59862
Kirill Khudyakov <kirill.hudiakov@chemopin.tech>
2017-10-01 17:48:10 +0300

    Add PeriodicTableModel

---
+++ PeriodicTable/PeriodicTable/Models/PeriodicTableModel.swift
@@ -0,0 +1,15 @@
+//
+//  PeriodicTableModel.swift
+//  PeriodicTable
+//
+//  Created by Kirill Khudyakov on 01.10.17.
+//  Copyright © 2017 adeveloper. All rights reserved.
+//
+
+import Foundation
+
+class PeriodicTableModel {
+    
+    var elements: [ElementTableModel] = []
+    
+}

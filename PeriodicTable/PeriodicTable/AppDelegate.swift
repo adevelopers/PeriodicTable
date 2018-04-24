c4dde14d4a5cee2ef0cfcdd13c3f295ff889d262
Kirill Khudyakov <hudiakov@gmail.com>
2018-04-24 16:01:05 +0300

    Remove Bugs from project

--- PeriodicTable/PeriodicTable/AppDelegate.swift
+++ PeriodicTable/PeriodicTable/AppDelegate.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-import Bugsee
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -23,7 +23,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         UINavigationBar.appearance().backIndicatorImage = #imageLiteral(resourceName: "button-back")
         UINavigationBar.appearance().backIndicatorTransitionMaskImage = UIImage()
         
-        Bugsee.launch(token :"13545456-68eb-43c2-b564-bfdeb167d37a")
         
         return true
     }
